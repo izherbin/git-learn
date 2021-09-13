e23e363e54bcb3f4e494038194be34bb648e529c
Maxim Piskunov <qualaddar@gmail.com>
2021-09-13 17:32:29 +0300

    Correct outsource

--- file1.js
+++ file1.js
@@ -5,7 +5,7 @@ someFunction()
 // Insert a little comment
 export function someFunction() {
 	console.log('It works!')
-	console.log('Addition made by outsource developer')
+	console.log('Addition made by outsource developer. CORRECTED!!!')
 	console.log(testFunction())
 }
 
